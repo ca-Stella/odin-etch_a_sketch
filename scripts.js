c376660bdaaedd74e40d8154c52b6b6b60bb3bb0
ca-Stella <stella.2532@gmail.com>
2022-07-08 10:09:15 -0600

    Set up hover effect that changes colour

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ const grid = document.getElementById('grid');
 let gridSize = 16;
 
 makeGrid();
+draw();
 
 // makeGrid() to create 16x16 grid of square divs
 function makeGrid() {
@@ -17,4 +18,17 @@ function makeGrid() {
             square_row.appendChild(square);
         }
     }
+}
+
+// draw() in each square when hovering
+function draw() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.addEventListener('mouseenter', fillColor));
+}
+
+// fillColor() in the square by changing the background colour
+function fillColor() {
+    color = 'black';
+    this.style.opacity = 1;
+    this.style.backgroundColor = color;
 }
